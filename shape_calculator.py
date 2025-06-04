1de1ac86d8f887205be65f03bb3d5bf1e514ab1a
Lord_Zane021 <140982529+Lord-Zane1225@users.noreply.github.com>
2025-06-04 13:30:27 +1200

    Add files via upload

---
+++ shape_calculator.py
@@ -0,0 +1,66 @@
+# functions
+
+def not_blank(question):
+    """Checks that a user response is not blank"""
+
+    while True:
+        response = input(question)
+
+        if response != "":
+            return response
+
+        print("Sorry, this can't be blank. Please try again. \n")
+
+
+def string_check(question, valid_ans_list):
+    """Checks that users enter the full world or the first letter of a word from a list of valid responses. """
+
+    while True:
+
+        response = input(question).lower()
+
+        for item in valid_ans_list:
+            if response == item:
+                return item
+
+            elif response == item[0]:
+                return item
+
+        print(f"Please choose an answer from {valid_ans_list}. ")
+
+
+def num_check(question, num_type, exit_code=None):
+    """Checks that users enter an integer / float more than 0 (or optional exit code)"""
+    if num_type == "integer":
+        error = "Oops - Please enter an integer (no decimals) more than 0"
+        change_to = int
+    else:
+        error = "Oops - Please enter a number more than 0"
+        change_to = float
+
+    while True:
+        response = input(question).lower()
+
+        # checks for the exit code
+        if response == exit_code:
+            return response
+        try:
+            # check response is an integer and is more than 0
+            response = change_to(response)
+
+            if response > 0:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+def shape_calc():
+    """Gets a shape from the user and finds either the area or perimeter of that shape. """
+    string_check("Please enter what shape you would like: ", shape_tuple)
+
+
+# main routine
+shape_tuple = ("square", "rectangle", "triangle", "circle")
